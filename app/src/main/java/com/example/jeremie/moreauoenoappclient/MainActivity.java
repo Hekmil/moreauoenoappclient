6bdfdc94ffd1b351f58ec44d08cc423b1117df31
Jeremie <j.poisson86@laposte.net>
2018-10-30 10:37:10 +0100

    No bar

--- app/src/main/java/com/example/jeremie/moreauoenoappclient/MainActivity.java
+++ app/src/main/java/com/example/jeremie/moreauoenoappclient/MainActivity.java
@@ -5,7 +5,6 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.view.Window;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -36,11 +35,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-
         setContentView(R.layout.activity_main);
-
-        //commentairequipue
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);

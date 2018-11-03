6d3f63c667bd97eb943b96b84e093119764a3c71
Pierre <pployet.ing2020@esaip.org>
2018-11-03 18:43:26 +0100

    Version 03/11/2018

--- app/src/main/java/com/example/jeremie/moreauoenoappclient/MainActivity.java
+++ app/src/main/java/com/example/jeremie/moreauoenoappclient/MainActivity.java
@@ -1,18 +1,37 @@
 package com.example.jeremie.moreauoenoappclient;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void pageajout(View view){
+        startActivity(new Intent(this, Ajout.class));
+    }
+
+    public void pageaconversion(View view){
+        startActivity(new Intent(this, Conversion.class));
+    }
+
+    public void pagesulfitage(View view){
+        startActivity(new Intent(this, Sulfitage.class));
+    }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         FragmentManager fragmentManager = getSupportFragmentManager();
+
+
+
+
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -46,8 +65,10 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
     }
 
 }

f4ea728403f947729a1a717035795f8c18336843
Unknown <j.poisson86@laposte.net>
2018-11-20 16:26:26 +0100

    New version

--- app/src/main/java/com/example/jeremie/moreauoenoappclient/Sulfitage.java
+++ app/src/main/java/com/example/jeremie/moreauoenoappclient/Sulfitage.java
@@ -2,12 +2,30 @@ package com.example.jeremie.moreauoenoappclient;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class Sulfitage extends AppCompatActivity {
+    private EditText textVolumeCuve;
+    private EditText textSO2Initial;
+    private EditText textSO2Souhaite;
+    private EditText textResultat;
+    private Spinner spinnerVolumeCuve;
+    private Spinner spinnerConcentration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sulfitage);
+        spinnerVolumeCuve = findViewById(R.id.spinnerVolumeCuve);
+        spinnerConcentration = findViewById(R.id.spinnerConcentration);
+        textVolumeCuve = findViewById(R.id.textVolumeCuve);
+        textSO2Initial = findViewById(R.id.textSO2Initial);
+        textSO2Souhaite = findViewById(R.id.textSO2Souhaite);
+        textResultat = findViewById(R.id.textResultat);
+        ArrayAdapter<CharSequence> adapterSpinnerVolumeCuve = ArrayAdapter.createFromResource(this, R.array.arrayVolumeCuve, android.R.layout.simple_spinner_item);
+
     }
+
 }

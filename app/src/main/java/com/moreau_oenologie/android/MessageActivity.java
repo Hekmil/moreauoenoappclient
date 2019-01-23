4b28d7f17d183a05e6be6d13363d0c1a87986972
clementb49 <clement.boussiron@gmail.com>
2019-01-23 21:36:57 +0100

    implement intent on message activity

--- app/src/main/java/com/moreau_oenologie/android/MessageActivity.java
+++ app/src/main/java/com/moreau_oenologie/android/MessageActivity.java
@@ -1,13 +1,24 @@
 package com.moreau_oenologie.android;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MessageActivity extends AppCompatActivity {
+	private User user;
+	private FirebaseUser firebaseUser;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_message);
+		firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+		Intent intent = getIntent();
+		if (intent!= null) {
+			user = intent.getParcelableExtra("user");
+		}
 	}
 }
